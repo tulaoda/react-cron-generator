bd3c15b14b3aef8fa39b627cb6683f0ae10aa476
宋鑫鑫 <songxinxin@songxinxindeMacBook-Pro.local>
2019-10-09 17:10:00 +0800

    feat(all):format code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react'
-import Cron from './lib'
+import React, { Component } from "react";
+import Cron from "./lib";
 class App extends Component {
-
   constructor(props) {
-      super(props);
-      this.state = {
-       
-      };
+    super(props);
+    this.state = {};
   }
 
   render() {
-    return (<div>
-      <Cron
-        onChange={(e)=> {this.setState({value:e}); console.log(e)}}
-        value={this.state.value}
-        showResultText={true}
-        showResultCron={true}
+    return (
+      <div>
+        <Cron
+          onChange={e => {
+            this.setState({ value: e });
+            console.log(e);
+          }}
+          value={this.state.value}
+          showResultText={true}
+          showResultCron={true}
         />
-                            
-    </div>)
+      </div>
+    );
   }
 }
 
